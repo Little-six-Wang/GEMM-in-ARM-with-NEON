a0cd5c60b8841544deeb976d867b98731cc23003
Little-six-Wang <1292828130@qq.com>
2024-06-02 21:52:14 +0800

    commit forth

---
+++ MMul/MMult1x2_4.c
@@ -0,0 +1,29 @@
+#define A(i, j) a[(j) * lda + (i)]
+#define B(i, j) b[(j) * ldb + (i)]
+#define C(i, j) c[(j) * ldc + (i)]
+
+void AddDot1x2(int, double*, int, double*, int, double*, int);
+void AddDot(int, double*, int, double*, double*);
+
+void MY_MMult(int m,  int n, int k, double* a, int lda, double* b, int ldb, double* c, int ldc){
+    int i, j;
+    for(j = 0; j < n; j += 2){
+        for(i = 0; i < m; i++){
+            AddDot1x2(k, &A(i, 0), lda, &B(0, j), ldb, &C(i, j), ldc);
+        }
+    }
+}
+
+void AddDot1x2(int k, double* a, int lda, double* b, int ldb, double* c, int ldc){
+    AddDot(k, &A(0, 0), lda, &B(0, 0), &C(0, 0));
+    AddDot(k, &A(0, 0), lda, &B(0, 1), &C(0, 1));
+}
+
+#define X(i) x[(i) * incx]
+
+void AddDot(int k, double* x, int incx, double* y, double* gamma){
+    int p;
+    for(p = 0; p < k; p++){
+        *gamma += X(p) * y[p];
+    }
+}
