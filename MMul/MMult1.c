582ab6a9f54f47121cfe58885e0e8d96996c4aa6
Little-six-Wang <1292828130@qq.com>
2024-06-02 19:55:06 +0800

    运行MMult1

--- MMul/MMult1.c
+++ MMul/MMult1.c
@@ -2,14 +2,22 @@
 #define B(i, j) b[j * ldb + i]
 #define C(i, j) c[j * ldc + i]
 
+void AddDot(int, double*, int, double*, double*);
+
 void MY_MMult(int m,  int n, int k, double* a, int lda, double* b, int ldb, double* c, int ldc){
-    int i, j, p;
-    for(i = 0; i < m; i++){
-        for(j = 0; j < n; j++){
-            for(p = 0; p < k; p++){
-                C(i, j) += A(i, p) * B(p, j);
-            }
+    int i, j;
+    for(j = 0; j < n; j++){
+        for(i = 0; i < m; i++){
+            AddDot(k, &A(i, 0), lda, &B(0, j), &C(i, j));
         }
     }
 }
 
+#define x(i) x[i * incx]
+
+void AddDot(int k, double* x, int incx, double* y, double* gamma){
+    int p;
+    for(p = 0; p < k; p++){
+        *gamma += x(p) * y[p];
+    }
+}
