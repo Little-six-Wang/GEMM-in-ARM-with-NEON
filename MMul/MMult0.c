43f74fba839eaaefeb81e455a5311340b7e4e49b
Little-six-Wang <1292828130@qq.com>
2024-06-02 19:23:09 +0800

    运行MMult0

--- MMul/MMult0.c
+++ MMul/MMult0.c
@@ -4,8 +4,8 @@
 
 void MY_MMult(int m,  int n, int k, double* a, int lda, double* b, int ldb, double* c, int ldc){
     int i, j, p;
-    for(i = 0; i < n; i++){
-        for(j = 0; j < m; j++){
+    for(i = 0; i < m; i++){
+        for(j = 0; j < n; j++){
             for(p = 0; p < k; p++){
                 C(i, j) += A(i, p) * B(p, j);
             }
