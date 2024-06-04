c15f0a6a62458d9378835d06ac31a0e94e1dd090
Little-six-Wang <1292828130@qq.com>
2024-06-04 09:09:49 +0800

    展开内积

---
+++ MMul/MMult1x4_5.c
@@ -0,0 +1,24 @@
+#define A(i, j) a[(j) * lda + (i)]
+#define B(i, j) b[(j) * ldb + (i)]
+#define C(i, j) c[(j) * ldc + (i)]
+
+void AddDot1x4(int, double*, int, double*, int, double*, int);
+
+void MY_MMult(int m,  int n, int k, double* a, int lda, double* b, int ldb, double* c, int ldc){
+    int i, j;
+    for(j = 0; j < n; j += 4){
+        for(i = 0; i < m; i++){
+            AddDot1x4(k, &A(i, 0), lda, &B(0, j), ldb, &C(i, j), ldc);
+        }
+    }
+}
+
+void AddDot1x4(int k, double* a, int lda, double* b, int ldb, double* c, int ldc){
+    int p;
+    for(p = 0; p < k; p++){
+        C(0, 0) += A(0, p) * B(p, 0);
+        C(0, 1) += A(0, p) * B(p, 1);
+        C(0, 2) += A(0, p) * B(p, 2);
+        C(0, 3) += A(0, p) * B(p, 3);
+    }
+}
