f4b7a66c2d0f0d758c21e17b8562b0802b5c66ce
Little-six-Wang <1292828130@qq.com>
2024-06-04 16:58:46 +0800

    neon

--- MMul/MMult1x4_10.c
+++ MMul/MMult1x4_10.c
@@ -16,34 +16,36 @@ void MY_MMult(int m,  int n, int k, double* a, int lda, double* b, int ldb, doub
 #include<arm_neon.h>
 #include<stdlib.h>
 
+typedef union{
+    float64x2_t v;
+    double d[2];
+}v2df_t;
+
+
 void AddDot1x4(int k, double* a, int lda, double* b, int ldb, double* c, int ldc){
     int p;
     double *b_p0_ptr, *b_p1_ptr, *b_p2_ptr, *b_p3_ptr;
-    double* c_result = (double*)malloc(4 * sizeof(double));
-    float64x2_t c_00_01_nreg, c_02_03_nreg;
-    float64x2_t a_0p_nreg;
-    float64x2_t b_p0_p1_nreg, b_p2_p3_nreg;
-    c_00_01_nreg = vmovq_n_f64(0.0);
-    c_02_03_nreg = vmovq_n_f64(0.0);
+    v2df_t c_00_01_nreg, c_02_03_nreg;
+    v2df_t a_0p_nreg;
+    v2df_t b_p0_p1_nreg, b_p2_p3_nreg;
+    c_00_01_nreg.v = vmovq_n_f64(0.0);
+    c_02_03_nreg.v = vmovq_n_f64(0.0);
     b_p0_ptr = &B(0, 0);
     b_p1_ptr = &B(0, 1);
     b_p2_ptr = &B(0, 2);
     b_p3_ptr = &B(0, 3);
-    for(p = 0; p < k; p += 4){
-        a_0p_nreg = vld1q_dup_f64(&A(0, p));
-        b_p0_p1_nreg = vld1q_lane_f64(b_p0_ptr++, b_p0_p1_nreg, 0);
-        b_p0_p1_nreg = vld1q_lane_f64(b_p1_ptr++, b_p0_p1_nreg, 1);
-        b_p2_p3_nreg = vld1q_lane_f64(b_p2_ptr++, b_p2_p3_nreg, 0);
-        b_p2_p3_nreg = vld1q_lane_f64(b_p3_ptr++, b_p2_p3_nreg, 1);
+    for(p = 0; p < k; p++){
+        a_0p_nreg.v = vld1q_dup_f64(&A(0, p));
+        b_p0_p1_nreg.v = vld1q_lane_f64(b_p0_ptr++, b_p0_p1_nreg.v, 0);
+        b_p0_p1_nreg.v = vld1q_lane_f64(b_p1_ptr++, b_p0_p1_nreg.v, 1);
+        b_p2_p3_nreg.v = vld1q_lane_f64(b_p2_ptr++, b_p2_p3_nreg.v, 0);
+        b_p2_p3_nreg.v = vld1q_lane_f64(b_p3_ptr++, b_p2_p3_nreg.v, 1);
 
-        c_00_01_nreg = vfmaq_f64(c_00_01_nreg, a_0p_nreg, b_p0_p1_nreg);
-        c_02_03_nreg = vfmaq_f64(c_02_03_nreg, a_0p_nreg, b_p2_p3_nreg);
+        c_00_01_nreg.v = vfmaq_f64(c_00_01_nreg.v, a_0p_nreg.v, b_p0_p1_nreg.v);
+        c_02_03_nreg.v = vfmaq_f64(c_02_03_nreg.v, a_0p_nreg.v, b_p2_p3_nreg.v);
     }
-    vst1q_f64(c_result, c_00_01_nreg);
-    vst1q_f64(c_result + 2, c_02_03_nreg);
-    C(0, 0) = c_result[0];
-    C(0, 1) = c_result[1];
-    C(0, 2) = c_result[2];
-    C(0, 3) = c_result[3];
-    free(c_result);
+    C(0, 0) += c_00_01_nreg.d[0];
+    C(0, 1) += c_00_01_nreg.d[1];
+    C(0, 2) += c_02_03_nreg.d[0];
+    C(0, 3) += c_02_03_nreg.d[1];
 }
