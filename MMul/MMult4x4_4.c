9b6db790f29e83e62b18e8deace7c6929440c468
Little-six-Wang <1292828130@qq.com>
2024-06-04 18:28:19 +0800

    MMult4x4_4

--- MMul/MMult4x4_4.c
+++ MMul/MMult4x4_4.c
@@ -15,26 +15,58 @@ void MY_MMult(int m, int n, int k, double* a, int lda, double* b, int ldb, doubl
 }
 
 void AddDot4x4(int k, double* a, int lda, double* b, int ldb, double* c, int ldc){
-    
-    AddDot(k, &A(0, 0), lda, &B(0, 0), &C(0, 0));
-    AddDot(k, &A(0, 0), lda, &B(0, 1), &C(0, 1));
-    AddDot(k, &A(0, 0), lda, &B(0, 2), &C(0, 2));
-    AddDot(k, &A(0, 0), lda, &B(0, 3), &C(0, 3));
+    int p;
+    for(p = 0; p < k; p++){
+        C(0, 0) += A(0, p) * B(p, 0);
+    }
+    for(p = 0; p < k; p++){
+        C(0, 1) += A(0, p) * B(p, 1);
+    }
+    for(p = 0; p < k; p++){
+        C(0, 2) += A(0, p) * B(p, 2);
+    }
+    for(p = 0; p < k; p++){
+        C(0, 3) += A(0, p) * B(p, 3);
+    }
 
-    AddDot(k, &A(1, 0), lda, &B(0, 0), &C(1, 0));
-    AddDot(k, &A(1, 0), lda, &B(0, 1), &C(1, 1));
-    AddDot(k, &A(1, 0), lda, &B(0, 2), &C(1, 2));
-    AddDot(k, &A(1, 0), lda, &B(0, 3), &C(1, 3));
+    for(p = 0; p < k; p++){
+        C(1, 0) += A(1, p) * B(p, 0);
+    }
+    for(p = 0; p < k; p++){
+        C(1, 1) += A(1, p) * B(p, 1);
+    }
+    for(p = 0; p < k; p++){
+        C(1, 2) += A(1, p) * B(p, 2);
+    }
+    for(p = 0; p < k; p++){
+        C(1, 3) += A(1, p) * B(p, 3);
+    }
 
-    AddDot(k, &A(2, 0), lda, &B(0, 0), &C(2, 0));
-    AddDot(k, &A(2, 0), lda, &B(0, 1), &C(2, 1));
-    AddDot(k, &A(2, 0), lda, &B(0, 2), &C(2, 2));
-    AddDot(k, &A(2, 0), lda, &B(0, 3), &C(2, 3));
+    for(p = 0; p < k; p++){
+        C(2, 0) += A(2, p) * B(p, 0);
+    }
+    for(p = 0; p < k; p++){
+        C(2, 1) += A(2, p) * B(p, 1);
+    }
+    for(p = 0; p < k; p++){
+        C(2, 2) += A(2, p) * B(p, 2);
+    }
+    for(p = 0; p < k; p++){
+        C(2, 3) += A(2, p) * B(p, 3);
+    }
 
-    AddDot(k, &A(3, 0), lda, &B(0, 0), &C(3, 0));
-    AddDot(k, &A(3, 0), lda, &B(0, 1), &C(3, 1));
-    AddDot(k, &A(3, 0), lda, &B(0, 2), &C(3, 2));
-    AddDot(k, &A(3, 0), lda, &B(0, 3), &C(3, 3));
+    for(p = 0; p < k; p++){
+        C(3, 0) += A(3, p) * B(p, 0);
+    }
+    for(p = 0; p < k; p++){
+        C(3, 1) += A(3, p) * B(p, 1);
+    }
+    for(p = 0; p < k; p++){
+        C(3, 2) += A(3, p) * B(p, 2);
+    }
+    for(p = 0; p < k; p++){
+        C(3, 3) += A(3, p) * B(p, 3);
+    }
 }
 
 #define X(i) x[(i) * incx]
